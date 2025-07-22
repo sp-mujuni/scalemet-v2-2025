8436d8004b5a52eec530ff547fc3a9982aa860d2
sp-mujuni <smujuni99@gmail.com>
2025-07-22 16:09:09 +0300

    Add Header component for displaying financial overview

---
+++ components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-white shadow p-4 mb-4">
+      <h2 className="text-lg font-semibold">Financial Overview</h2>
+    </header>
+  );
+}
